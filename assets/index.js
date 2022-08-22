b581d1ecfb5d801cb553cf932574d3e819a9eff2
Phil Branch <philbranch243@gmail.com>
2022-08-22 10:23:05 -0500

    layout html, then will write questions, then javascript for quiz

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+    // EventListener,
+        //click even t function call
+// THEN a timer starts and I am presented with a question
+    // function
+        //timer function and innerhtml/append
+// WHEN I answer a question
+    //click event
+
+// THEN I am presented with another question
+    //function
+        //append
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+    //function if/else
+        //innerhtml compared to correct answer
+// WHEN all questions are answered or the timer reaches 0
+    //if/else
+        //one in our timer, check against 0
+        // is my question array empty?
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+    //text entry
+    //localstorage
+    
+
